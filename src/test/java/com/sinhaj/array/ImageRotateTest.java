78ea070db090a1e35ca5f13ac81ac42b1e109ace
ajaysinha26 <ajaysinha26@gmail.com>
2013-09-02 20:42:54 +0530

    Image Rotate Test

---
+++ src/test/java/com/sinhaj/array/ImageRotateTest.java
@@ -0,0 +1,11 @@
+package com.sinhaj.array;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ajaysinha
+ * Date: 9/2/13
+ * Time: 8:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageRotateTest {
+}
